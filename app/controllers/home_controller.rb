2aa14e2e7ebfc46db01be9dd0ddb2badad7cca21
no-excuse-makoto <e1621220@gmail.com>
2024-12-16 18:21:31 +0900

    create about

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  #URL(home/top)に対応するHTMLが来てtopアクションはtopビューを見つけ出している
   def top
   end
+
+  #homeコントローラーの２つ目だからgenerate controllerは使えない
+  def about
+  end
+  
 end
